ad5d45467a0f2a537c5563ce0c0380d2c3d7d46c
Gabriel Oliveira <ras.vibpositive@gmail.com>
2018-07-31 18:51:30 +0100

    Initial Commit

---
+++ application/controllers/Home.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Recipe class file
+ *
+ * PHP Version 5.2
+ *
+ * @category Recipe
+ * @package  Recipe
+ * @author   Lorna Jane Mitchell <lorna@ibuildings.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://example.com/recipes
+ */
+ 
+/**
+ * Recipe class
+ *
+ * The class holding the root Recipe class definition
+ *
+ * @category Recipe
+ * @package  Recipe
+ * @author   Lorna Jane Mitchell <lorna@ibuildings.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://example.com/recipes/recipe
+ */
+
+
+class Home extends CI_Controller
+{
+    //Rota no caso eh view
+    /**
+     * Renders the view
+     *
+     * This function calls a static fetching method against the Ingredient class
+     * and returns everything matching this recipe ID
+     *
+     * @param string $page The position of the current token in the
+     *
+     * @return string blank
+     */
+    public function index($page = 'home')
+    {
+        if (!file_exists(APPPATH.'views/home/'.$page.'.php')) {
+            show_404();
+        }
+
+        $data['title'] = ucfirst($page);
+        $this->load->view('templates/header');
+        $this->load->view('pages/'.$page, $data);
+        $this->load->view('templates/footer');
+    }
+}
