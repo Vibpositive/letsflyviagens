3cddd7cc4a5eeb36a401332e57d60487262d654f
Gabriel Oliveira <ras.vibpositive@gmail.com>
2018-08-05 21:57:43 +0100

    Adds News controller and Model

---
+++ application/controllers/News.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Recipe class file
+ *
+ * PHP Version 5.2
+ *
+ * @category Recipe
+ * @package  Recipe
+ * @author   Lorna Jane Mitchell <lorna@ibuildings.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://example.com/recipes
+ */
+ 
+/**
+ * Recipe class
+ *
+ * The class holding the root Recipe class definition
+ *
+ * @category Recipe
+ * @package  Recipe
+ * @author   Lorna Jane Mitchell <lorna@ibuildings.com>
+ * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
+ * @link     http://example.com/recipes/recipe
+ */
+
+
+class Home extends CI_Controller
+{
+    //Rota no caso eh view
+    /**
+     * Renders the view
+     *
+     * This function calls a static fetching method against the Ingredient class
+     * and returns everything matching this recipe ID
+     *
+     * @param string $page The position of the current token in the
+     *
+     * @return string blank
+     */
+    public function index($page = 'index')
+    {
+        if (!file_exists(APPPATH.'views/home/'.$page.'.php')) {
+            show_404();
+        }
+        
+
+        $data['title'] = ucfirst($page);
+        $data['home'] = $this->uri->segment(1) == '';
+
+        $this->load->model('news_name');
+        
+    }
+}
---
+++ application/models/News_model.php
@@ -0,0 +1,42 @@
+<?php 
+
+class News_model extends CI_Model
+{
+    public $id;
+    public $title;
+    public $body;
+    public $date;
+
+    public function __construct(){
+        $this->load->database();
+
+    }
+
+    public function getnews()
+    {
+        $query = $this->db->get('news', 10);
+        // print_r($query->result_array());
+        $result = $query->result();
+        echo random_element($result)->title;
+
+        return $query->result();
+    }
+
+    public function insert_entry()
+    {
+        $this->title    = $_POST['title']; // please read the below note
+        $this->body  = $_POST['body'];
+        $this->date     = time();
+
+        $this->db->insert('entries', $this);
+    }
+
+    public function update_entry()
+    {
+        $this->title    = $_POST['title'];
+        $this->body  = $_POST['body'];
+        $this->date     = time();
+
+        $this->db->update('entries', $this, array('id' => $_POST['id']));
+    }
+}
