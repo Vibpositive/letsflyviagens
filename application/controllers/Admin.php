b77d190b33f5d500655326caa3da642f04dc123b
Gabriel Oliveira <ras.vibpositive@gmail.com>
2018-08-27 22:22:44 +0100

    removes unnecessary lines

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -1,49 +1,14 @@
 <?php
 
-/**
- * Recipe class file
- *
- * PHP Version 5.2
- *
- * @category Recipe
- * @package  Recipe
- * @author   Lorna Jane Mitchell <lorna@ibuildings.com>
- * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
- * @link     http://example.com/recipes
- */
- 
-/**
- * Recipe class
- *
- * The class holding the root Recipe class definition
- *
- * @category Recipe
- * @package  Recipe
- * @author   Lorna Jane Mitchell <lorna@ibuildings.com>
- * @license  http://opensource.org/licenses/gpl-license.php GNU Public License
- * @link     http://example.com/recipes/recipe
- */
-
-
 class Admin extends CI_Controller
 {
-    //Rota no caso eh view
-    /**
-     * Renders the view
-     *
-     * This function calls a static fetching method against the Ingredient class
-     * and returns everything matching this recipe ID
-     *
-     * @param string $page The position of the current token in the
-     *
-     * @return string blank
-     */
-                                                        
+                                             
     public function index($page = 'index')
     {
         if (!file_exists(APPPATH.'views/admin/'.$page.'.php')) {
             // show_404();
         }
+
         $this->load->view('templates/admin/header');
         $this->load->view('admin/menu');
         $this->load->view('admin/index');
@@ -70,21 +35,20 @@ class Admin extends CI_Controller
                     }
                     break;
                     case 'delete':
-                    if($this->model->delete($id)){
-                        redirect("admin/home/" . $page, 'refresh');
-                    }else{
-                        // TODO: better message
-                        die("could not delete");
-                    }
-                    break;
-                    case 'create':
-                    // if($this->model->create($post)){
-                    if($this->model->create($post)){
-                        redirect("admin/home/" . $page, 'refresh');
-                    }else{
-                        die("could not create");
-                        // die("could not update");
-                    }
+                        if($this->model->delete($id)){
+                            redirect("admin/home/" . $page, 'refresh');
+                        }else{
+                            // TODO: better message
+                            die("could not delete");
+                        }
+                        break;
+                        case 'create':
+                        if($this->model->create($post)){
+                            redirect("admin/home/" . $page, 'refresh');
+                        }else{
+                            // TODO: better message
+                            die("could not create");
+                        }
                     break;
                 default:
                     # code...
