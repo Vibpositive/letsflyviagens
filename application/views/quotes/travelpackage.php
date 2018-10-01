7bd4bcbb8729ccce71dbec36a28ebedf457256d9
Gabriel Oliveira <ras.vibpositive@gmail.com>
2018-10-01 16:17:33 +0100

    fixes name on departureCity

--- application/views/quotes/travelpackage.php
+++ application/views/quotes/travelpackage.php
@@ -33,7 +33,7 @@
                         <label for="tp_phone">Telefone</label>
                         <input type="tel" id="tp_phone" name="tp_phone" placeholder="+55(XX)XXXX-XXXX" required="">
                         
-                        <label for="tp_departureCity">Destino</label>
+                        <label for="tp_departureCity">Cidade de Saida</label>
                         <input type="text" id="tp_departureCity" name="tp_departureCity" placeholder="Cidade de Saida" class="form-control" required="">
 
                         <label for="tp_destinationCity" id="tp_destinationCity">Destino</label>
