ecc3ad595d170497738b0f36b9b0937033921570
Gabriel Oliveira <ras.vibpositive@gmail.com>
2019-02-17 04:29:21 +0000

    Refactors typer_model

--- application/models/admin/Typer_model.php
+++ application/models/admin/Typer_model.php
@@ -9,58 +9,91 @@ Class Typer_model extends CI_Model {
 	}
 	
 	public function get() {
-		$this->db->select('*');
-		$this->db->from($this -> table);
-		$query = $this->db->get();
-		return $query -> result_array();
+		try {
+			$this->db->select('*');
+			$this->db->from($this -> table);
+			$query = $this->db->get();
+			
+			$result = $query -> result_array();
+
+            $db_error = $this->db->error();
+            if ($db_error['code'] != 0) {
+				throw new Exception('Database error! Error Code [' . $db_error['code'] . '] Error: ' . $db_error['message']);
+            }
+			return $result;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
 	}
 	
 	public function get_by_id($id = 0) {
-		$this->db->select('*');
-		$this->db->where("id", $id);
-		$this->db->from($this -> table);
-		$query = $this->db->get();
-		return $query -> result_array();
+		try {
+			$this->db->select('*');
+			$this->db->where("id", $id);
+			$this->db->from($this -> table);
+			$query = $this->db->get();
+			
+			$result = $query -> result_array();
+
+            $db_error = $this->db->error();
+            if ($db_error['code'] != 0) {
+				throw new Exception('Database error! Error Code [' . $db_error['code'] . '] Error: ' . $db_error['message']);
+            }
+			return $result;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
 	}
 	
 	public function update($id, $data) {
 		try {
 			$this->db->where("id", $id);
 			$this->db->update($this -> table, $data);
-			$db_error = $this->db->error();
 			
-			if ($db_error['code'] != 0) {
+			$result = $this->db->affected_rows() == 1;
+
+            $db_error = $this->db->error();
+            if ($db_error['code'] != 0) {
 				throw new Exception('Database error! Error Code [' . $db_error['code'] . '] Error: ' . $db_error['message']);
-			}
-			
-			return $this->db->affected_rows() == 1;
-		} catch (Exception $e) {
-			return $e->getMessage();
-		}
+            }
+			return $result;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
 	}
 	
 	public function delete($id) {
-		// TODO: update accordingly to create method
-		$this->db->where("id", $id);
-		$this->db->delete($this -> table);
-		return $this->db->affected_rows() == 1;
+		try {
+			$this->db->where("id", $id);
+			$this->db->delete($this -> table);
+			
+			$result = $this->db->affected_rows() == 1;
+
+            $db_error = $this->db->error();
+            if ($db_error['code'] != 0) {
+				throw new Exception('Database error! Error Code [' . $db_error['code'] . '] Error: ' . $db_error['message']);
+            }
+			return $result;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
 	}
 	
 	public function create($data) {
 		try {
 			$this->db->insert($this -> table, $data);
-			$db_error = $this->db->error();
 			
-			if ($db_error['code'] != 0) {
+			$result = $this->db->insert_id();
+
+            $db_error = $this->db->error();
+            if ($db_error['code'] != 0) {
 				throw new Exception('Database error! Error Code [' . $db_error['code'] . '] Error: ' . $db_error['message']);
-			}
-			return $this->db->insert_id();
-		} catch (Exception $e) {
-			return $e->getMessage();
-		}
+            }
+			return $result;
+        } catch (Exception $e) {
+            return $e->getMessage();
+        }
 	}
-	
-	
 }
 
 ?>
